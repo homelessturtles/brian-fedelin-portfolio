d07a0fc36f6bd4b8e66227ff4f78a3aa4db73300
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2022-07-27 16:29:15 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import Home from './components/Home'
-import Projects from './components/Projects';
-import About from './components/About';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}> 
-        <Route index element={<Projects />}/>
-      </Route> 
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
